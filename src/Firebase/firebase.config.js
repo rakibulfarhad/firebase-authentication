b5d57212c762c76a9acc8278afbdd8934ad00c8e
rakibulfarhad <mraisfa@gmail.com>
2021-10-07 01:02:25 +0600

    first commit

---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDZrj8D30IupnY2uelzA5axw2z8zKiVAkM",
+    authDomain: "fir-authentication-242db.firebaseapp.com",
+    projectId: "fir-authentication-242db",
+    storageBucket: "fir-authentication-242db.appspot.com",
+    messagingSenderId: "722736588280",
+    appId: "1:722736588280:web:28c536e4bb5995f7e3220d"
+  };
+
+  export default firebaseConfig;
